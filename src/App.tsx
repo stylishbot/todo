03b6cafc576a48462c8a956be0f48c7f9b2f0f5e
stylishbot <c.110225@icloud.com>
2022-03-31 23:20:17 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import TaskList from './components/TaskList'
+import TaskInput from './components/TaskInput'
+import { Task } from './Types'
+import './App.css';
+
+const initialState: Task[]=[
+  {
+    id: 2,
+    title: '次のTODO',
+    done: false
+  },{
+    id: 1,
+    title: '最初のTODO',
+    done: true
+  }
+]
+
+const App: React.FC=()=>{
+  const[tasks, setTasks]=useState(initialState)
+  return(
+    <div>
+      <TaskInput tasks={tasks} setTasks={setTasks}/>
+      <TaskList tasks={tasks} setTasks={setTasks}/>
+    </div>
+  )
+}
+export default App
